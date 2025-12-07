94ec14f63d9509353161a91e0697d82a0d43496d
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-12-07 23:12:38 +0530

    db

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
+
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT||3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+if (process.env.NODE_ENV === "production") job.start();
+
+// middleware
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
-app.get("/api",(req,res)=>{
-            res.send("Got it")
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
 });
