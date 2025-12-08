0b7f1bad6b07804e924bb3ecb822f562a591d179
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-12-08 14:47:47 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
-import job from "./config/cron.js";
+
 
 dotenv.config();
 const app = express();
